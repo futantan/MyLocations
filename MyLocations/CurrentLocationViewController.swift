3f106227f936bfaa181d39fff13590e6eb8e1853
futantan <luckytantanfu@gmail.com>
2015-07-07 16:52:50 +0800

    IBOutlet & IBAction

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
